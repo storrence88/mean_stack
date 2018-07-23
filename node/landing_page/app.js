fcc3632089ec7ea37c580e97baaf97b19f804e1b
Steven Torrence <storrence88@gmail.com>
2018-07-22 23:18:17 -0500

    Added views folder

--- node/landing_page/app.js
+++ node/landing_page/app.js
@@ -3,21 +3,21 @@ const fs = require('fs');
 const server = http.createServer((request, response) => {
   console.log('Client request URL: ', request.url);
   if(request.url === '/') {
-    fs.readFile('index.html', 'utf8', (errors, contents) => {
+    fs.readFile('views/index.html', 'utf8', (errors, contents) => {
       response.writeHead(200, {'Content-Type': 'text/html'});
       response.write(contents);
       response.end();
     });
   }
   else if(request.url === '/ninjas') {
-    fs.readFile('ninjas.html', 'utf8', (errors, contents) => {
+    fs.readFile('views/ninjas.html', 'utf8', (errors, contents) => {
       response.writeHead(200, {'Content-Type': 'text/html'});
       response.write(contents);
       response.end();
     });
   }
   else if(request.url === '/dojos/new') {
-    fs.readFile('dojos.html', 'utf8', (errors, contents) => {
+    fs.readFile('views/dojos.html', 'utf8', (errors, contents) => {
       response.writeHead(200, {'Content-Type': 'text/html'});
       response.write(contents);
       response.end();
